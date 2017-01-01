504906e492ae0669609a6d5e9e070945768d816b
Lee Saville <l.w.saville@gmail.com>
2017-01-01 15:28:56 -0700

    Add intitialize method with ivars for the attributes I'm expecting

--- app/models/weather_day.rb
+++ app/models/weather_day.rb
@@ -1,3 +1,11 @@
 class WeatherDay
-
+  attr_reader :weekday, :month, :date, :high, :low, :conditions
+  def initialize(raw_weather)
+    @weekday    = raw_weather[:date][:weekday]
+    @month      = raw_weather[:date][:monthname]
+    @date       = raw_weather[:date][:day]
+    @high       = raw_weather[:high]
+    @low        = raw_weather[:low] 
+    @conditions = raw_weather[:conditions]
+  end
 end
