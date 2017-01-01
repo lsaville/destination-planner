1179393e304b0aed6026a72e353cb5d4773e1286
Lee Saville <l.w.saville@gmail.com>
2017-01-01 15:29:48 -0700

    Add test for .create_days

--- spec/models/10_day_forecast_spec.rb
+++ spec/models/10_day_forecast_spec.rb
@@ -21,5 +21,44 @@ describe 'weather_day' do
       expect(day.low).to eq("27")
       expect(day.conditions).to eq("Clear")
     end
+
+    context '.create_days' do
+      it 'rolls through a collection and makes itself' do
+        weather_info1 = {
+                         :date => {:weekday => 'Tuesday',
+                                   :monthname => 'January',
+                                   :day => '10'
+                                   },
+                         :high => '49',
+                         :low  => '27',
+                         :conditions => 'Clear'
+                        }
+        
+        weather_info2 = {
+                         :date => {:weekday => 'Wednesday',
+                                   :monthname => 'January',
+                                   :day => '11'
+                                   },
+                         :high => '56',
+                         :low  => '23',
+                         :conditions => 'Cloudy'
+                        }
+        
+        several_days = [weather_info1, weather_info2]
+
+        weather_days = WeatherDay.create_days(several_days)
+        day1 = weather_days.first
+        day2 = weather_days.last
+        
+        expect(day1).to be_a(WeatherDay)
+        expect(day2).to be_a(WeatherDay)
+        expect(day2.weekday).to eq("Wednesday")
+        expect(day2.month).to eq("January")
+        expect(day2.date).to eq("11")
+        expect(day2.high).to eq("56")
+        expect(day2.low).to eq("23")
+        expect(day2.conditions).to eq("Cloudy")
+      end
+    end
   end
 end
