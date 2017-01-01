029b68ff3c281babf3f755bf36fef2dfd6fea631
Lee Saville <l.w.saville@gmail.com>
2017-01-01 15:54:33 -0700

    Add class method to manufacture instances of self

--- app/models/weather_day.rb
+++ app/models/weather_day.rb
@@ -8,4 +8,10 @@ class WeatherDay
     @low        = raw_weather[:low] 
     @conditions = raw_weather[:conditions]
   end
+
+  def self.create_days(array_of_raw_weather)
+    array_of_raw_weather.map do |raw_weather|
+      WeatherDay.new(raw_weather)
+    end
+  end
 end
