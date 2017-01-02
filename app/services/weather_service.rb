5095240218135d8ee7a2751f408d9b797cc8fc28
Lee Saville <l.w.saville@gmail.com>
2017-01-01 17:34:06 -0700

    Add ten_day_forecast method with dynamic zipcode

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,2 +1,13 @@
 class WeatherService
+  def initialize(zipcode)
+    @zipcode = zipcode
+  end
+
+  def ten_day_forecast
+    response = Faraday.get("http://api.wunderground.com/api/#{ENV["weather_api_key"]}/forecast10day/q/#{zipcode}.json")
+    JSON.parse(response.body, symbolize_names: true)[:forecast][:simpleforecast][:forecastday]
+  end
+
+  private 
+    attr_reader :zipcode
 end
