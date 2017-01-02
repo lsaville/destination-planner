11115bd21b26b095b901a5a5b984b3a366764fa3
Lee Saville <l.w.saville@gmail.com>
2017-01-01 17:30:38 -0700

    Change test to pass zipcode to the weatherservice instead of consuming raw hand-rolled hash

--- spec/models/weather_day_spec.rb
+++ spec/models/weather_day_spec.rb
@@ -23,41 +23,19 @@ describe 'weather_day' do
     end
 
     context '.create_days' do
-      it 'rolls through a collection and makes itself' do
-        weather_info1 = {
-                         :date => {:weekday => 'Tuesday',
-                                   :monthname => 'January',
-                                   :day => '10'
-                                   },
-                         :high => {:fahrenheit =>'49', :celsius => '3'},
-                         :low  => {:fahrenheit => '27', :celsius => '3'},
-                         :conditions => 'Clear'
-                        }
-        
-        weather_info2 = {
-                         :date => {:weekday => 'Wednesday',
-                                   :monthname => 'January',
-                                   :day => '11'
-                                   },
-                         :high => {:fahrenheit =>'56', :celsius => '3'},
-                         :low  => {:fahrenheit => '23', :celsius => '3'},
-                         :conditions => 'Cloudy'
-                        }
-        
-        several_days = [weather_info1, weather_info2]
-
-        weather_days = WeatherDay.create_days(several_days)
+      it 'rolls through a collection and makes itself', :vcr do
+        weather_days = WeatherDay.create_days('80205')
         day1 = weather_days.first
         day2 = weather_days.last
         
         expect(day1).to be_a(WeatherDay)
         expect(day2).to be_a(WeatherDay)
-        expect(day2.weekday).to eq("Wednesday")
+        expect(day2.weekday).to eq("Tuesday")
         expect(day2.month).to eq("January")
-        expect(day2.date).to eq("11")
-        expect(day2.high).to eq("56")
-        expect(day2.low).to eq("23")
-        expect(day2.conditions).to eq("Cloudy")
+        expect(day2.date).to eq(10)
+        expect(day2.high).to eq("50")
+        expect(day2.low).to eq("26")
+        expect(day2.conditions).to eq("Clear")
       end
     end
   end
